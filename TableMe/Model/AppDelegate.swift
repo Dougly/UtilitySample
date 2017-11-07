515eaf4dcf3b536141437109b757236dc6c29165
Dougly <dgalante11@gmail.com>
2017-11-07 11:59:49 -0500

    crashlytics script and api key added

--- TableMe/Model/AppDelegate.swift
+++ TableMe/Model/AppDelegate.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
+        Fabric.with([Crashlytics.self])
         // Override point for customization after application launch.
         return true
     }
