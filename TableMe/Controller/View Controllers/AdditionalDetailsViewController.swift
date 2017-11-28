1239c16468f5ff06eb83e5d0df4725dde7e254b3
Dougly <dgalante11@gmail.com>
2017-11-28 11:05:34 -0500

    gender picker added

--- TableMe/Controller/View Controllers/AdditionalDetailsViewController.swift
+++ TableMe/Controller/View Controllers/AdditionalDetailsViewController.swift
@@ -14,44 +14,26 @@ class AdditionalDetailsViewController: UIViewController {
         return .lightContent
     }
     
+    let genderOptions = ["Male", "Female", "Other"]
+    
     @IBOutlet weak var scrollViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var additionalDetailsView: EditProfileView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let datePicker = UIDatePicker()
-        datePicker.datePickerMode = .date
-        datePicker.backgroundColor = .black
-        datePicker.setValue(false, forKeyPath: "highlightsToday")
-        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
-        additionalDetailsView.emailTextField.inputView = datePicker
-        
-        let toolBar = UIToolbar()
-        toolBar.barStyle = .blackTranslucent
-        toolBar.sizeToFit()
-        
-        let doneButton = UIBarButtonItem(title: "Select", style: .done, target: self, action: #selector(handleDoneButton))
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancelButton))
-        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-        toolBar.isUserInteractionEnabled = true
-        additionalDetailsView.emailTextField.inputAccessoryView = toolBar
-        
-        
-    }
-    
-    @objc func handleCancelButton() {
-    
-    }
-    
-    
-    @objc func handleDoneButton() {
-        
+        scrollView.alwaysBounceVertical = true
+        setupDatePicker()
+        setupGenderPicker()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: Notification.Name.UIKeyboardWillHide, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: Notification.Name.UIKeyboardWillShow, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
     }
     
     @objc func keyboardWillAppear(notification: NSNotification) {
@@ -60,16 +42,97 @@ class AdditionalDetailsViewController: UIViewController {
             let keyboardHeight = keyboardRectangle.height
             scrollViewBottomConstraint.constant = keyboardHeight * -1
         }
-        //Do something here
     }
     
     @objc func keyboardWillDisappear() {
         scrollViewBottomConstraint.constant = 0
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
-    }
+ 
+    
+   
   
+    
+}
+
+// MARK: PickerViews
+extension AdditionalDetailsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    //Date Picker
+    func setupDatePicker() {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.backgroundColor = .black
+        datePicker.setValue(false, forKeyPath: "highlightsToday")
+        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+        datePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+        additionalDetailsView.birthdayTextField.inputView = datePicker
+        
+        let birthdayToolBar = UIToolbar()
+        birthdayToolBar.barStyle = .blackTranslucent
+        birthdayToolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(handleBirthdayDoneButton))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        birthdayToolBar.setItems([spaceButton, doneButton], animated: false)
+        birthdayToolBar.isUserInteractionEnabled = true
+        additionalDetailsView.birthdayTextField.inputAccessoryView = birthdayToolBar
+    }
+    
+    @objc func handleBirthdayDoneButton(_ sender: UIButton) {
+        additionalDetailsView.birthdayTextField.resignFirstResponder()
+    }
+    
+    @objc func dateChanged(_ sender: UIDatePicker) {
+        let componenets = Calendar.current.dateComponents([.year, .month, .day], from: sender.date)
+        if let day = componenets.day, let month = componenets.month, let year = componenets.year {
+            additionalDetailsView.birthdayTextField.text = "\(month) / \(day) / \(year)"
+        }
+    }
+    
+    //Gender Picker
+    func setupGenderPicker() {
+        let genderPicker = UIPickerView()
+        genderPicker.dataSource = self
+        genderPicker.delegate = self
+        genderPicker.backgroundColor = .black
+        genderPicker.setValue(UIColor.white, forKeyPath: "textColor")
+        additionalDetailsView.genderTextField.inputView = genderPicker
+        
+        let genderToolBar = UIToolbar()
+        genderToolBar.barStyle = .blackTranslucent
+        genderToolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(handleGenderDoneButton))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancelButton))
+        genderToolBar.setItems([spaceButton, doneButton], animated: false)
+        genderToolBar.isUserInteractionEnabled = true
+        additionalDetailsView.genderTextField.inputAccessoryView = genderToolBar
+        
+    }
+    
+    @objc func handleGenderDoneButton() {
+        additionalDetailsView.genderTextField.resignFirstResponder()
+        additionalDetailsView.birthdayTextField.becomeFirstResponder()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genderOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titleData = genderOptions[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 20),NSAttributedStringKey.foregroundColor:UIColor.white])
+        return myTitle
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        additionalDetailsView.genderTextField.text = genderOptions[row]
+    }
+    
     
 }
